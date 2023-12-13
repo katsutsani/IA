76ec288a80e350503820aa8d674ae444a1b196c5
billyboyy <92396476+billyboyy@users.noreply.github.com>
2023-12-13 16:43:08 +0100

    modif

--- Assets/Input System/Bird Monster/MonsterBird.cs
+++ Assets/Input System/Bird Monster/MonsterBird.cs
@@ -4,62 +4,60 @@ using UnityEngine;
 
 public class BirdInputSystem : MonoBehaviour
 {
-    private Rigidbody monsterBody;
     public float speed = 1.0f;
     private Animator animator;
     private Vector2 moveMonster = Vector2.up;
-    Transform _transform;
-    [SerializeField] Transform[] _wayPoint;
+    private Transform transformMonster;
+    [SerializeField] Transform[] wayPoint;
 
-    private bool _isWaiting = true;
-    private float _waitTime = 1.0f;
-    private float _waitCounter = 0.0f;
+    private bool isWaiting = true;
+    private float waitTime = 1.0f;
+    private float waitCounter = 0.0f;
 
-    private int _currentWayPointIndex = 0;
+    private int currentWayPointIndex = 0;
 
     private void Awake()
     {
-        monsterBody = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
-        _transform = GetComponent<Transform>();
+        transformMonster = GetComponent<Transform>();
     }
 
     private void Update()
-    {
-        MonsterMove();
-    }
-
-    void MonsterMove()
     {
         CheckWayPoints();
     }
 
     void CheckWayPoints()
     {
-        if (_isWaiting)
+        if (isWaiting)
         {
-            _waitCounter += Time.deltaTime;
-            if (_waitCounter >= _waitTime)
+            waitCounter += Time.deltaTime;
+            if (waitCounter >= waitTime)
             {
-                _isWaiting = false;
+                isWaiting = false;
             }
         }
         else
         {
-            Transform wp = _wayPoint[_currentWayPointIndex];
-            if (Vector3.Distance(_transform.position, wp.position) < 1f)
+            Transform wp = wayPoint[currentWayPointIndex];
+            if (Vector3.Distance(transformMonster.position, wp.position) < 1f)
             {
                 animator.SetBool("isWalking", false);
-                _waitCounter = 0.0f;
-                _isWaiting = true;
-                _currentWayPointIndex = (_currentWayPointIndex + 1) % _wayPoint.Length;
+                waitCounter = 0.0f;
+                isWaiting = true;
+                currentWayPointIndex = (currentWayPointIndex + 1) % wayPoint.Length;
             }
             else
             {
                 animator.SetBool("isWalking", true);
-                _transform.position = Vector3.MoveTowards(_transform.position, wp.position, speed * Time.deltaTime);
+                transformMonster.position = Vector3.MoveTowards(transformMonster.position, wp.position, speed * Time.deltaTime);
             }
         }
+    }
+
+    void checkPlayer()
+    {
+
     }
 
 }
