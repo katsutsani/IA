585080c7a7bc2bf780554cdd36ea2eda52bdc3e4
Guillaume <71400691+katsutsani@users.noreply.github.com>
2023-12-21 21:59:32 +0100

    update map + collision

--- Assets/Scripts/Gameplay/checkCollision.cs
+++ Assets/Scripts/Gameplay/checkCollision.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class checkCollision : MonoBehaviour
 {
     [SerializeField] ParticleSystem _hitEnnemyParticle;
+    [SerializeField] ParticleSystem _hitWalls;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Monsters") || other.gameObject.layer == LayerMask.NameToLayer("Boss"))
         {
-            Destroy(other.gameObject);
             var Particle = Instantiate(_hitEnnemyParticle, other.gameObject.transform.position, other.gameObject.transform.rotation);
+            Destroy(other.gameObject);
+            Particle.Play();
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Walls"))
+        {
+            var Particle = Instantiate(_hitWalls, other.gameObject.transform.position, other.gameObject.transform.rotation);
             Particle.Play();
         }
 
